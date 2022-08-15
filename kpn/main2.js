6e524854b488bbad3eaea7da801e93b7e4465ac2
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2022-08-15 17:09:56 +0200

    Add files via upload

--- kpn/main2.js
+++ kpn/main2.js
@@ -5,7 +5,7 @@ let computer_choice_id = document.getElementById("computer_choice");
 let user_choice_id = document.getElementById("user_choice");
 let computer_score_id = document.getElementById("computer_score");
 let user_score_id = document.getElementById("user_score");
-
+const table = document.getElementById("history")
 
 let choices_object = {
     'K': {
@@ -44,31 +44,12 @@ function checker(input) {
             gameEvaluation('DRAW', input, computer_choice, counter++)
             break;
     }
-    computer_score_id.innerHTML = computer_score;
-    user_score_id.innerHTML = user_score;
-
-    if(computer_score === 5) {
-        alert('prehral si')
-        if (window.confirm('chcete hru spustiť znova?') == true) {
-        resetGame()
-        } else {
-            alert('')
-        }
-    } 
-    else if(user_score === 5) {
-        alert('vyhral si')
-        if (window.confirm('chcete hru spustiť znova?') == true) {
-        resetGame()   
-        } else {
-            alert('')
-        }
-    }
+    checkWinner() 
 }
-
-const table = document.getElementById("history")
 
 function createRow(rank, user, computer, winner) {
     const row = table.insertRow()
+    row.classList.add('newRow')
     const rankCell = row.insertCell()
     const userCell = row.insertCell()
     const computerCell = row.insertCell()
@@ -86,16 +67,51 @@ function gameEvaluation(result, input, computer_choice, counter) {
     result_ref.innerHTML = result
     result === 'YOU LOSE' ? computer_score++ : result === 'YOU WIN' ? user_score++ : ''
     createRow(counter, input, computer_choice, result)
+    computer_score_id.innerHTML = computer_score;
+    user_score_id.innerHTML = user_score;
 }
 
 function resetGame() {
-    // spytať sa mareka ako vymazať všetk rows cez deleteRow()
-    table.remove();
-    //location.reload();
+    //console.log(document.getElementsByClassName('newRow'))
+    const newRows = document.getElementsByClassName('newRow')
+
+    while(newRows.length > 0){
+        newRows[0].remove();
+    }
+    
     computer_choice_id.innerHTML=''
     user_choice_id.innerHTML=''
     computer_score_id.innerHTML='0'
     user_score_id.innerHTML='0'
+    computer_score='0'
+    user_score='0'
     result_ref.innerHTML=''
     result_ref.classList.value= ''
+    counter='1'
 }
+
+function checkWinner() {
+    if(user_score === 3) {
+        showAlert('vyhral si')
+    }
+    else if(computer_score === 3) {
+        showAlert('prehral si')
+    }
+}
+
+function showAlert(text) {
+    setTimeout(() => {
+        alert(text)
+        window.confirm('chcete hru spustiť znova?') 
+        resetGame()
+    }, 100);
+}
+
+//showalert - bude zobrazovať alerty so settimeout
+//checkwinner -  bude vyhodnocovať logiku a zavola funkciu showalert
+
+/* else if(user_score === 3) {
+    alert('vyhral si')
+    window.confirm('chcete hru spustiť znova?') 
+    resetGame()
+} */
